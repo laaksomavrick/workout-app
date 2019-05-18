06b05de30cff9951f3d8488d088003c470ade7cd
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-05-18 00:55:31 -0400

    lift show; update

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -2,7 +2,26 @@ class LiftsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @lifts = current_user.lifts.all
+    @lifts = current_user.lifts.all.order(:name)
+  end
+
+  def show
+    id = params[:id]
+    @lift = Lift.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @lift = Lift.find(id)
+    @lift.update(lift_params)
+    flash[:success] = "#{@lift.name} updated."
+    redirect_to @lift
+  end
+
+  private
+
+  def lift_params
+    params.require(:lift).permit(:name, :one_rep_max)
   end
 
 end
